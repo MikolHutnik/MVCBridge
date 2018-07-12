d24b500c64076afa003d8b1ec96af9ac2fc9cb25
MikolHutnik <mikolhutnik@gmail.com>
2018-07-12 12:14:12 -0400

    menu

---
+++ MVCBridge/Models/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBridge.Models
+{
+    public class Menu
+    {
+        [Key]
+
+        public string Item { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
